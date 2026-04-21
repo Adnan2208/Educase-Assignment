90692c858c89817dc60faa6eea131d5f5c5b5eb3
Adnan <adnan.c2208@gmail.com>
2026-04-21 23:23:07 +0530

    Added env changes for railway deployment

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { initDatabase } from './db';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.MYSQLPORT || 3000;
 
 app.use(express.json());
 
--- src/db.ts
+++ src/db.ts
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool: Pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'school_db',
+  host: process.env.MYSQLHOST || 'localhost',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQL_ROOT_PASSWORD || '',
+  database: process.env.MYSQL_DATABASE || 'school_db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 10,
